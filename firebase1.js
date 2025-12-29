dc35ce939514527356204745909829c9c6020d06
unknown <206084850+igtseth@users.noreply.github.com>
2025-12-29 14:06:31 +0300

    Add files via upload
    Signed-off-by: unknown <206084850+igtseth@users.noreply.github.com>

---
+++ firebase1.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCQzgTCwKhuXQreCvRm4wHJXNDtUHnGDEw",
+  authDomain: "postpage-b6fab.firebaseapp.com",
+  projectId: "postpage-b6fab",
+  storageBucket: "postpage-b6fab.firebasestorage.app",
+  messagingSenderId: "570490230784",
+  appId: "1:570490230784:web:b125636fe7dcd9d237e0e5"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
