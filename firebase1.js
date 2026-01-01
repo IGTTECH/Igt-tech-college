79b95d60341b0e7c1041aa3f772a22c5928c1877
unknown <206084850+igtseth@users.noreply.github.com>
2026-01-01 14:57:49 +0300

    Update firebase1.js
    Signed-off-by: unknown <206084850+igtseth@users.noreply.github.com>

--- firebase1.js
+++ firebase1.js
@@ -1,3 +1,4 @@
+
 const firebaseConfig = {
   apiKey: "AIzaSyCQzgTCwKhuXQreCvRm4wHJXNDtUHnGDEw",
   authDomain: "postpage-b6fab.firebaseapp.com",
@@ -7,8 +8,17 @@ const firebaseConfig = {
   appId: "1:570490230784:web:b125636fe7dcd9d237e0e5"
 };
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+
+const appCheck = firebase.appCheck();
+
+appCheck.activate(
+  "6LcOCj0sAAAAAF0N0SZ1QFMqFq7FEh-lIdpzaum-", // 🔴 PASTE FROM FIREBASE CONSOLE
+  true // Auto-refresh tokens
+);
 
